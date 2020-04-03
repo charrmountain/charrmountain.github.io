4a2b6565df36ce16774b174b7333dcfb011aef92
Charlotte Mountain <charr.mt@gmail.com>
2020-04-02 21:19:41 -0700

    updates

--- script.js
+++ script.js
@@ -5,28 +5,3 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
-
-  
-  $(document).ready(function(){
-    // Activate carousel
-    $("#myCarousel").carousel();
-    
-    // Enable carousel control
-    $(".carousel-control-prev").click(function(){
-        $("#myCarousel").carousel('prev');
-    });
-    $(".carousel-control-next").click(function(){
-        $("#myCarousel").carousel('next');
-    });
-    
-    // Enable carousel indicators
-    $(".slide-one").click(function(){
-        $("#myCarousel").carousel(0);
-    });
-    $(".slide-two").click(function(){
-        $("#myCarousel").carousel(1);
-    });
-    $(".slide-three").click(function(){
-        $("#myCarousel").carousel(2);
-    });
-});
