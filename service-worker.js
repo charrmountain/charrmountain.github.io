a1e1cadf3b4a954980811a512c9c97c7f506cfab
Charlotte Mountain <charr.mt@gmail.com>
2022-08-28 17:10:50 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9540c1034fe58dc42c94b09c91edfd97.js"
+  "/precache-manifest.6dae242e8f05158b481f0e89adb4b019.js"
 );
 
 self.addEventListener('message', (event) => {
