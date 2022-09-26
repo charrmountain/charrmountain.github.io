6624a995451ae7fc917175c5325cdda843638fa3
Charlotte Mountain <charr.mt@gmail.com>
2022-09-26 16:51:37 -0500

    JS

--- src/components/Pages/Skills/Skills.js
+++ src/components/Pages/Skills/Skills.js
@@ -57,17 +57,17 @@ export default class Skills extends Component {
         jQuery("#skills .projectRelation span").removeClass("fadeIn");
       },
     });
-    function moveExperience(){
-      jQuery('#skills .wrapper').append(jQuery('#skills .projectRelation'));
-    }
-    jQuery( document ).ready(function() {
-        moveExperience();
-    });
-    jQuery( window ).resize(function() {
+    function moveExperience() {
       var viewportWidth = jQuery(window).width();
-      if(viewportWidth < 750){
-        moveExperience();
+      if (viewportWidth < 750) {
+        jQuery("#skills .wrapper").append(jQuery("#skills .projectRelation"));
       }
+    }
+    jQuery(document).ready(function () {
+      moveExperience();
+    });
+    jQuery(window).resize(function () {
+      moveExperience();
     });
   }
 
